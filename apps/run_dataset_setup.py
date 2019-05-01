9d563dc60f050c299bb812e1ebcf3867943e8584
tommasomartini <tommaso.martini91@gmail.com>
2019-05-01 18:40:59 +0200

    Check that base data dir exists

--- apps/run_dataset_setup.py
+++ apps/run_dataset_setup.py
@@ -1,9 +1,15 @@
+import os
+
 import mnist.preparation.data_download as d_dload
 from mnist.config import GeneralConfig
 
 
 def main():
-    d_dload.download_mnist_data(data_dir=GeneralConfig.DATA_DIR,
+    data_dir = GeneralConfig.DATA_DIR
+    if not os.path.exists(data_dir):
+        raise IOError('Base data folder {} does not exist'.format(data_dir))
+
+    d_dload.download_mnist_data(data_dir=data_dir,
                                 silent=False,
                                 dry=True)
 
