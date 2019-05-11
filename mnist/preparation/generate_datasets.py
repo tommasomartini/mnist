b656a1a38445499d1613e4cefaa9ee01289efb4d
tommasomartini <tommaso.martini91@gmail.com>
2019-05-11 16:40:21 +0200

    Ultimated generate datasets module

--- mnist/preparation/generate_datasets.py
+++ mnist/preparation/generate_datasets.py
@@ -1,42 +1,148 @@
-import glob
 import json
 import os
+import random
 
+import numpy as np
 from tqdm import tqdm
 
+import mnist.constants as constants
 import mnist.file_interface as fi
-from mnist.config import GeneralConfig
-from mnist.constants import Constants
+from mnist.custom_utils.logger import data_setup_logger as logger
+
+_SPLIT_NAME_TO_FILENAME = {
+    constants.Constants.TRAINING_SET_NAME: constants.Filenames.TRAINING_SET,
+    constants.Constants.VALIDATION_SET_NAME: constants.Filenames.VALIDATION_SET,
+    constants.Constants.TEST_SET_NAME: constants.Filenames.TEST_SET,
+}
 
 
-def generate_datasets(data_dir):
-    # The metadata file pattern should be something like '*.json'.
-    meta_file_pattern = '*' + os.path.extsep + Constants.METADATA_EXTENSION
+def _gather_samples_by_class(data_dir, silent=False):
+    """Returns a dict of all the sample ids, divided by class label.
 
-    # Relative path like: '*/*/*'.
-    # Add as many '*' as the number of subfolders.
-    hash_subdir_pattern = tuple([
-        '*' for _
-        in range(GeneralConfig.NUM_HASH_SUBDIR_LEVELS)
-    ])
+    Args:
+        data_dir (str): Path to the base data folder to load the samples from.
+        silent (bool, optional): If True, no message is print out. Defaults to
+            False.
 
-    # Put the path pattern together into something like:
-    #   /path/to/data/*/*/*.json
-    path_pieces = (data_dir,) + hash_subdir_pattern + (meta_file_pattern,)
-    meta_path_pattern = os.path.join(*path_pieces)
-    all_meta_paths = glob.glob(meta_path_pattern)
+    Returns:
+        A dict mapping a class label to all the sample ids belonging to that
+        class.
+    """
+    all_meta_paths = fi.get_all_metadata_filepaths_from_dir(data_dir)
 
-    samples_by_label = {}
+    logger.debug('{} files to fetch from disk'.format(len(all_meta_paths)))
+
+    class_samples = {}
     for meta_path in tqdm(all_meta_paths,
-                          desc='Collecting all samples'):
+                          desc='Collecting all samples',
+                          disable=silent):
         with open(meta_path, 'r') as f:
             meta = json.load(f)
 
         sample_id = fi.MetadataReader.get_id(meta)
         sample_label = fi.MetadataReader.get_label(meta)
 
-        sample_list = samples_by_label.setdefault(sample_label, [])
-        sample_list.append(sample_id)
+        sample_set = class_samples.setdefault(sample_label, set())
+        sample_set.add(sample_id)
 
-    for label, samples in samples_by_label.items():
-        print('{}: {} samples'.format(label, len(samples)))
+    return class_samples
+
+
+def generate_datasets(data_dir,
+                      dataset_def_dir,
+                      data_split_weights,
+                      silent=False,
+                      dry=False):
+    if set(data_split_weights.keys()) != set(_SPLIT_NAME_TO_FILENAME.keys()):
+        raise ValueError('Names of the split sets do not match the known names'
+                         'for filename mapping')
+
+    if not os.path.exists(data_dir):
+        raise IOError('Folder {} does not exist'.format(data_dir))
+
+    if not os.path.exists(dataset_def_dir):
+        raise IOError('Folder {} does not exist'.format(dataset_def_dir))
+
+    if dry:
+        logger.warning('Dry run!')
+
+    class_samples = _gather_samples_by_class(data_dir, silent)
+
+    # Convert the data split weights into floats summing up to 1.
+    sum_weights = sum(list(data_split_weights.values()))
+    data_split_fracs = {
+        split_name: float(frac) / sum_weights
+        for split_name, frac
+        in data_split_weights.items()
+    }
+
+    logger.debug('Data splits')
+    for split_name, split_frac in data_split_fracs.items():
+        logger.debug('  {}: {}'.format(split_name, split_frac))
+
+    datasets = {
+        split_name: set()
+        for split_name in data_split_fracs.keys()
+    }
+    for label, sample_ids in class_samples.items():
+        logger.debug('Class {}: {} samples'.format(label, len(sample_ids)))
+        _for_class_label = 'for_class_label'
+        logger.push(_for_class_label).add()
+
+        num_samples = len(sample_ids)
+        assigned_sample_ids = set()
+        for split_idx,\
+            (split_name, frac) in enumerate(data_split_fracs.items()):
+            logger.debug(split_name)
+            logger.push(split_name).add()
+
+            split_size = int(np.round(frac * num_samples))
+
+            logger.debug('{} samples to assign'.format(split_size))
+
+            if split_size == 0:
+                raise ValueError('0 samples in {}. Consider adding samples '
+                                 'or using a different '
+                                 'dataset split'.format(split_name))
+
+            unassigned_sample_ids = sample_ids - assigned_sample_ids
+
+            logger.debug('{} unassigned samples'
+                         ''.format(len(unassigned_sample_ids)))
+
+            if split_idx == len(datasets) - 1:
+                # Last split: use all the remaining samples.
+                logger.debug('Last split {}: assign all '
+                             'the remaining samples'.format(split_name))
+                datasets[split_name].update(unassigned_sample_ids)
+            else:
+                split_samples = random.sample(unassigned_sample_ids, split_size)
+                assigned_sample_ids.update(split_samples)
+                datasets[split_name].update(split_samples)
+
+                logger.debug('Split {}: '
+                             'assign {} samples'.format(split_name,
+                                                        len(split_samples)))
+
+            logger.pop(split_name)
+
+        logger.pop(_for_class_label)
+
+    # Save the datasets on disk.
+    for split_name, sample_ids in datasets.items():
+        logger.info('Dataset {}: {} samples'.format(split_name,
+                                                    len(sample_ids)))
+
+        filename = _SPLIT_NAME_TO_FILENAME.get(split_name, None)
+        if filename is None:
+            raise ValueError('Unknown filename for split {}'.format(split_name))
+
+        dataset_path = os.path.join(dataset_def_dir, filename)
+        if os.path.exists(dataset_path):
+            raise ValueError('Dataset {} already exists'.format(dataset_path))
+
+        sorted_sample_ids = sorted(list(sample_ids))
+        if not dry:
+            with open(dataset_path, 'w') as f:
+                json.dump(sorted_sample_ids, f, indent=4)
+        logger.info('Dataset {} stored at {}'.format(split_name, dataset_path))
---
+++ mnist/preparation/generate_datasets_tests.py
@@ -0,0 +1,39 @@
+import json
+import unittest
+
+from pyfakefs import fake_filesystem_unittest
+
+import mnist.constants as constants
+import mnist.file_interface as fi
+import mnist.preparation.generate_datasets as gd
+
+
+class TestGatherSamplesByClass(fake_filesystem_unittest.TestCase):
+
+    def setUp(self):
+        self.setUpPyfakefs()
+        self._data_dir = '/my/base/dir'
+
+        self._class_samples = {
+            0: {'foo', 'bar'},
+            1: {'baz'},
+        }
+
+        for label, sample_ids in self._class_samples.items():
+            for sample_id in sample_ids:
+                meta = {
+                    constants.MetadataFields.ID: sample_id,
+                    constants.MetadataFields.LABEL: label,
+                }
+                meta_path = fi.get_metadata_path(self._data_dir, sample_id)
+                self.fs.create_file(meta_path,
+                                    contents=json.dumps(meta))
+
+    def test_success(self):
+        class_samples = gd._gather_samples_by_class(self._data_dir,
+                                                    silent=True)
+        self.assertDictEqual(class_samples, self._class_samples)
+
+
+if __name__ == '__main__':
+    unittest.main()
