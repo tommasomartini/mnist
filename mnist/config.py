f105841cf60023162626a2499e91905f1b3e6a30
tommasomartini <tommaso.martini91@gmail.com>
2019-05-11 16:37:34 +0200

    Add data split weights to config

--- mnist/config.py
+++ mnist/config.py
@@ -3,12 +3,21 @@ This module contains all the settings and configurations.
 """
 import logging
 
+import mnist.constants as constants
 import mnist.custom_utils.readonly as ro
 
 
 class GeneralConfig(ro.ReadOnly):
     """Container for general configurations."""
     RANDOM_SEED = 0
-    DATA_DIR = '/home/tom/Data/mnist'
+    DATA_DIR = ''
     NUM_HASH_SUBDIR_LEVELS = 2
     LOGGING_LEVEL = logging.INFO
+
+    # Datasets.
+    DATASET_DEF_DIR = ''
+    DATA_SPLIT_WEIGHTS = {
+        constants.Constants.TRAINING_SET_NAME: 0.5,
+        constants.Constants.VALIDATION_SET_NAME: 0.25,
+        constants.Constants.TEST_SET_NAME: 0.25,
+    }
