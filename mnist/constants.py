470b434b2c5718439148af5eaeba1854dc156493
tommasomartini <tommaso.martini91@gmail.com>
2019-05-11 14:30:14 +0200

    Add data split names

--- mnist/constants.py
+++ mnist/constants.py
@@ -3,12 +3,17 @@ This module contains all the constants used in this repo.
 
 You should not need to modify this file (not too often, at least!).
 """
+import os
+
 import mnist.custom_utils.readonly as ro
 
 
 class Constants(ro.ReadOnly):
     IMAGE_EXTENSION = 'png'
     METADATA_EXTENSION = 'json'
+    TRAINING_SET_NAME = 'training_set'
+    VALIDATION_SET_NAME = 'validation_set'
+    TEST_SET_NAME = 'test_set'
 
 
 class MetadataFields(ro.ReadOnly):
@@ -17,9 +22,13 @@ class MetadataFields(ro.ReadOnly):
 
 
 class Filenames(ro.ReadOnly):
-    TRAINING_SET = 'training_set.json'
-    VALIDATION_SET = 'validation_set.json'
-    TEST_SET = 'test_set.json'
+    # Should be something like: "dataset_spit.json".
+    TRAINING_SET = os.path.extsep.join((Constants.TRAINING_SET_NAME,
+                                        Constants.METADATA_EXTENSION))
+    VALIDATION_SET = os.path.extsep.join((Constants.VALIDATION_SET_NAME,
+                                          Constants.METADATA_EXTENSION))
+    TEST_SET = os.path.extsep.join((Constants.TEST_SET_NAME,
+                                    Constants.METADATA_EXTENSION))
 
 
 class LoggerNames(ro.ReadOnly):
