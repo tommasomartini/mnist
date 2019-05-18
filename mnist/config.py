cd42d809af2980e9fa82f8eee42e43b83752287a
tommasomartini <tommaso.martini91@gmail.com>
2019-05-18 12:52:48 +0200

    Log dir in config module

--- mnist/config.py
+++ mnist/config.py
@@ -10,22 +10,29 @@ import mnist.custom_utils.readonly as ro
 class GeneralConfig(ro.ReadOnly):
     """Container for general configurations."""
     RANDOM_SEED = 0
-    DATA_DIR = ''
+
+    # Where data are stored.
+    DATA_DIR = '/home/tom/Data/mnist'
+
+    # Where the output of the experiments will be stored.
+    LOG_DIR = '/home/tom/workspace/mnist/experiments'
+
     NUM_HASH_SUBDIR_LEVELS = 2
     LOGGING_LEVEL = logging.INFO
 
     # Datasets.
-    DATASET_DEF_DIR = ''
+    DATASET_DEF_DIR = '/home/tom/workspace/mnist/datasets'
     DATA_SPLIT_WEIGHTS = {
         constants.Constants.TRAINING_SET_NAME: 0.5,
         constants.Constants.VALIDATION_SET_NAME: 0.25,
         constants.Constants.TEST_SET_NAME: 0.25,
     }
 
+
 class TrainingConfig(ro.ReadOnly):
     """Container for training configurations."""
     NUM_EPOCHS = 50
-    BATCH_SIZE_TRAINING = 100
+    BATCH_SIZE_TRAINING = 32
     BATCH_SIZE_VALIDATION = BATCH_SIZE_TRAINING
     BATCH_SIZE_TEST = BATCH_SIZE_TRAINING
     LEARNING_RATE = 1e-3
