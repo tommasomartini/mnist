50c8d2803b0fa1e06bce9b80a345d283f3de0043
tommasomartini <tommaso.martini91@gmail.com>
2019-05-18 12:54:02 +0200

    MetaGraphs and Checkpoints paths

--- mnist/paths.py
+++ mnist/paths.py
@@ -5,6 +5,7 @@ import mnist.constants as constants
 import mnist.custom_utils.readonly as ro
 
 _dataset_def_dir = config.GeneralConfig.DATASET_DEF_DIR
+_log_dir = config.GeneralConfig.LOG_DIR
 
 
 class DatasetDefinitions(ro.ReadOnly):
@@ -21,3 +22,14 @@ class DatasetDefinitions(ro.ReadOnly):
         _dataset_def_dir,
         constants.DatasetFilenames[constants.Constants.TEST_SET_NAME]
     )
+
+
+class MetaGraphs(ro.ReadOnly):
+    """Stores the paths to the MetaGraph files, containing the definitions
+    of different graphs."""
+    TRAINING = os.path.join(_log_dir, constants.MetagraphFilenames.TRAINING)
+
+
+class Checkpoints(ro.ReadOnly):
+    """Stores the paths to the checkpoints generated during an experiment."""
+    TRAINING = os.path.join(_log_dir, constants.Constants.TRAINING_CKPT_NAME)
