e43a4597e1da19badc129af4bbc482516fd2b8b1
tommasomartini <tommaso.martini91@gmail.com>
2019-05-19 20:58:39 +0200

    Training utils functions

--- mnist/ml/training_utils.py
+++ mnist/ml/training_utils.py
@@ -1,33 +1,130 @@
-from itertools import chain
-from itertools import islice
-from itertools import starmap
-
 import numpy as np
 
 import mnist.constants as constants
+from mnist.custom_utils.logger import std_logger as logger
 
 
-# def batches_per_epoch(dataset_size, batch_size, drop_last=True):
-#     float_batches_over_epoch = float(dataset_size) / batch_size
-#     if drop_last:
-#         num_batches_per_epoch = int(np.floor(float_batches_over_epoch))
-#     else:
-#         num_batches_per_epoch = int(np.ceil(float_batches_over_epoch))
-#     return num_batches_per_epoch
+_CNST = constants.Constants
 
 
-# def epoch_cursor(epoch_idx, batch_idx, num_batches_per_epoch):
-#     epoch_cursor_multiplier = constants.Constants.EPOCH_CURSOR_MULTIPLIER
-#     if num_batches_per_epoch >= epoch_cursor_multiplier:
-#         raise RuntimeError('The number of batches per epoch must be smaller than the epoch '
-#                            'cursor multiplier: {} vs {}'.format(num_batches_per_epoch,
-#                                                                 epoch_cursor_multiplier))
-#     num_done_batches = (epoch_idx * num_batches_per_epoch) + batch_idx + 1
-#     fraction_done_epochs = float(num_done_batches) / num_batches_per_epoch
-#     epoch_cursor = int(np.round(fraction_done_epochs * epoch_cursor_multiplier))
-#     return epoch_cursor
+def batches_per_epoch(dataset_size, batch_size, drop_last):
+    """Computes the number of entire batches in one epoch.
+
+    Args:
+        dataset_size (int): Number of samples in the dataset.
+        batch_size (int): Size of each batch.
+        drop_last (bool): If True, the final reminder samples are dropped if
+            they are not enough to make an entire batch. Otherwise they are
+            considered to be part of the last batch.
+
+    Returns:
+        The number of entire batches in the epoch.
+
+    Raises:
+        ValueError: Batch size larger than the dataset size.
+    """
+    if batch_size > dataset_size:
+        raise ValueError('Batch size larger than the dataset size '
+                         '({} > {})'.format(batch_size, dataset_size))
+
+    if dataset_size == batch_size:
+        logger.warning('Dataset and batch have the same size: '
+                       '{}'.format(batch_size))
+
+    float_batches_over_epoch = float(dataset_size) / batch_size
+    if drop_last:
+        num_batches_per_epoch = int(np.floor(float_batches_over_epoch))
+    else:
+        num_batches_per_epoch = int(np.ceil(float_batches_over_epoch))
+    return num_batches_per_epoch
+
+
+def epoch_cursor(epoch_idx, batch_idx, batches_per_epoch):
+    """Returns an epoch cursor given the batch index in the current epoch.
+
+    A cursor is a progressive integer number uniquely denoting the latest
+    trained batch, such that the cursor associated with the last batch of each
+    epoch is a multiple of a given "cursor multiplier".
+
+    Examples:
+        multiplier = 1000
+        batches_per_epoch = 20
+
+        epoch_idx = 0   # first epoch
+        batch_idx = 0   # first batch
+        cursor is 1000 / 20 = 50: trained the first batch of the first epoch
+
+        epoch_idx = 0   # first epoch
+        batch_idx = 19   # last batch
+        cursor is 1000: trained the last batch of the first epoch
+
+        epoch_idx = 3   # forth epoch
+        batch_idx = 13   # 14th batch
+        cursor is 3700: trained the 14th batch of the 4th epoch
+            3000 units from the first 3 epochs
+            +
+            14 * (1000 / 20) units from the current epoch, including
+                             the just-trained batch
+
+    Args:
+        epoch_idx (int): 0-based index of the current epoch.
+        batch_idx (int): 0-based index of the current batch within the
+            current epoch.
+        batches_per_epoch (int): Number of batches in the current epoch.
+
+    Returns:
+        A integer number representing a cursor.
+
+    Raises:
+        ValueError: Index cannot be negative.
+        ValueError: Invalid number of batches per epoch.
+        ValueError: Number of batches per epoch larger than cursor multiplier.
+        ValueError: Batch index exceeds number of batches per epoch.
+    """
+    multip = _CNST.EPOCH_CURSOR_MULTIPLIER
+
+    if batch_idx < 0:
+        raise ValueError('Negative batch index: {}'.format(batch_idx))
+
+    if epoch_idx < 0:
+        raise ValueError('Negative epoch index: {}'.format(epoch_idx))
+
+    if batches_per_epoch < 0:
+        raise ValueError('Number of batches per epoch cannot be negative. '
+                         'Provided was: {}'.format(batches_per_epoch))
+
+    if batches_per_epoch > multip:
+        raise ValueError('Number of batches per epoch larger than '
+                         'cursor multiplier ({} > {})'.format(batches_per_epoch,
+                                                              multip))
+
+    if batch_idx >= batches_per_epoch:
+        raise ValueError('Batch index {} exceeds number of batches '
+                         'per epoch {}.'.format(batch_idx, batches_per_epoch))
+
+    units_past_epochs = epoch_idx * multip
+    units_per_batch = multip / batches_per_epoch
+    units_current_epoch = (batch_idx + 1) * units_per_batch
+    float_cursor = units_past_epochs + units_current_epoch
+    cursor = int(np.round(float_cursor))
+    return cursor
+
 
 def end_of_epoch_cursor(epoch_idx):
-    end_of_epoch_cursor = \
-        int((epoch_idx + 1) * constants.Constants.EPOCH_CURSOR_MULTIPLIER)
-    return end_of_epoch_cursor
+    """Returns the cursor relative to the latest trained epoch.
+
+    Args:
+        epoch_idx (int): 0-based index of the latest trained epoch.
+
+    Returns:
+        The cursor relative to the latest trained epoch.
+
+    Raises:
+        ValueError: Negative epoch index.
+    """
+    if epoch_idx < 0:
+        raise ValueError('Epoch index cannot be negative. '
+                         'Provided was: {}'.format(epoch_idx))
+
+    cursor = int((epoch_idx + 1) * _CNST.EPOCH_CURSOR_MULTIPLIER)
+    return cursor
---
+++ mnist/ml/training_utils_tests.py
@@ -0,0 +1,157 @@
+import unittest
+
+import mock
+
+import mnist.ml.training_utils as training_utils
+
+
+class TestBatchesPerEpoch(unittest.TestCase):
+
+    def test_drop_last_success(self):
+        dataset_size = 100
+        batch_size = 9
+        drop_last = True
+        expected_num_batches = 11
+
+        num_batches = training_utils.batches_per_epoch(
+            dataset_size=dataset_size,
+            batch_size=batch_size,
+            drop_last=drop_last)
+
+        self.assertEqual(num_batches, expected_num_batches)
+
+    def test_keep_last_success(self):
+        dataset_size = 100
+        batch_size = 9
+        drop_last = False
+        expected_num_batches = 12
+
+        num_batches = training_utils.batches_per_epoch(
+            dataset_size=dataset_size,
+            batch_size=batch_size,
+            drop_last=drop_last)
+
+        self.assertEqual(num_batches, expected_num_batches)
+
+    def test_batch_size_larger_than_dataset(self):
+        dataset_size = 10
+        batch_size = 100
+        drop_last = True
+
+        with self.assertRaises(ValueError):
+            training_utils.batches_per_epoch(
+                dataset_size=dataset_size,
+                batch_size=batch_size,
+                drop_last=drop_last)
+
+
+class TestEpochCursor(unittest.TestCase):
+
+    @mock.patch('mnist.ml.training_utils._CNST')
+    def test_first_batch_success(self, cursor_multiplier_mock):
+        cursor_multiplier_mock.EPOCH_CURSOR_MULTIPLIER = 1000
+        epoch_idx = 0
+        batch_idx = 0
+        batches_per_epoch = 20
+        expected_cursor = 50
+
+        cursor = training_utils.epoch_cursor(
+            epoch_idx=epoch_idx,
+            batch_idx=batch_idx,
+            batches_per_epoch=batches_per_epoch)
+
+        self.assertEqual(cursor, expected_cursor)
+
+    @mock.patch('mnist.ml.training_utils._CNST')
+    def test_last_batch_success(self, cursor_multiplier_mock):
+        cursor_multiplier_mock.EPOCH_CURSOR_MULTIPLIER = 1000
+        epoch_idx = 0
+        batches_per_epoch = 20
+        batch_idx = batches_per_epoch - 1
+        expected_cursor = 1000
+
+        cursor = training_utils.epoch_cursor(
+            epoch_idx=epoch_idx,
+            batch_idx=batch_idx,
+            batches_per_epoch=batches_per_epoch)
+
+        self.assertEqual(cursor, expected_cursor)
+
+    @mock.patch('mnist.ml.training_utils._CNST')
+    def test_middle_batch_success(self, cursor_multiplier_mock):
+        cursor_multiplier_mock.EPOCH_CURSOR_MULTIPLIER = 1000
+        epoch_idx = 3
+        batch_idx = 13
+        batches_per_epoch = 20
+        expected_cursor = 3700
+
+        cursor = training_utils.epoch_cursor(
+            epoch_idx=epoch_idx,
+            batch_idx=batch_idx,
+            batches_per_epoch=batches_per_epoch)
+
+        self.assertEqual(cursor, expected_cursor)
+
+    def test_negative_batch_index(self):
+        epoch_idx = 3
+        batch_idx = -13
+        batches_per_epoch = 20
+        with self.assertRaises(ValueError):
+            training_utils.epoch_cursor(epoch_idx=epoch_idx,
+                                        batch_idx=batch_idx,
+                                        batches_per_epoch=batches_per_epoch)
+
+    def test_negative_epoch_index(self):
+        epoch_idx = -3
+        batch_idx = 13
+        batches_per_epoch = 20
+        with self.assertRaises(ValueError):
+            training_utils.epoch_cursor(epoch_idx=epoch_idx,
+                                        batch_idx=batch_idx,
+                                        batches_per_epoch=batches_per_epoch)
+
+    def test_invalid_number_of_batches_per_epoch(self):
+        epoch_idx = 3
+        batch_idx = 13
+        batches_per_epoch = -20
+        with self.assertRaises(ValueError):
+            training_utils.epoch_cursor(epoch_idx=epoch_idx,
+                                        batch_idx=batch_idx,
+                                        batches_per_epoch=batches_per_epoch)
+
+    @mock.patch('mnist.ml.training_utils._CNST')
+    def test_batches_per_epoch_larger_than_multiplier(self,
+                                                      cursor_multiplier_mock):
+        cursor_multiplier_mock.EPOCH_CURSOR_MULTIPLIER = 1000
+        epoch_idx = 3
+        batch_idx = 13
+        batches_per_epoch = 2000
+        with self.assertRaises(ValueError):
+            training_utils.epoch_cursor(epoch_idx=epoch_idx,
+                                        batch_idx=batch_idx,
+                                        batches_per_epoch=batches_per_epoch)
+
+    def test_batch_index_exceeds_batches_per_epoch(self):
+        epoch_idx = 3
+        batch_idx = 130
+        batches_per_epoch = 20
+        with self.assertRaises(ValueError):
+            training_utils.epoch_cursor(epoch_idx=epoch_idx,
+                                        batch_idx=batch_idx,
+                                        batches_per_epoch=batches_per_epoch)
+
+
+class TestEndOfEpochCursor(unittest.TestCase):
+
+    @mock.patch('mnist.ml.training_utils._CNST')
+    def test_success(self, cursor_multiplier_mock):
+        cursor_multiplier_mock.EPOCH_CURSOR_MULTIPLIER = 1000
+        epoch_idx = 3   # end of the 4th epoch
+        expected_cursor = 4000
+        cursor = training_utils.end_of_epoch_cursor(epoch_idx=epoch_idx)
+        self.assertEqual(cursor, expected_cursor)
+
+    def test_negative_epoch_index(self):
+        epoch_idx = -1
+        with self.assertRaises(ValueError):
+            training_utils.end_of_epoch_cursor(epoch_idx=epoch_idx)
